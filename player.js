8f81a602013209bbaa1f50cbbe3c327f8757a762
genegwells21 <46447933+genegwells21@users.noreply.github.com>
2019-12-17 19:20:00 -0500

    Update player.js

--- player.js
+++ player.js
@@ -1,1 +1,15 @@
-
+function Player(){
+  this.w = 15;
+  this.h = 80;
+  
+  this.pos = createVector(this.w*2, height/2-this.h/2);
+  this.acc = createVector(0, 0);
+  this.spd = 10;
+  this.maxSpd = 10;
+  
+  this.show = function()  {
+    noStroke();
+    fill(255);
+    rect(this.pos.x, this.pos.y, this.w, this.h);
+  }
+}
